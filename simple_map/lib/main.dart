068b0a132ba5438ad685797e075f14a1d7249fc2
Yoochanhong <chanhongy6@naver.com>
2022-10-01 18:00:07 +0900

    객체에 맵핑을 위한 factory 사용

--- simple_map/lib/main.dart
+++ simple_map/lib/main.dart
@@ -2,6 +2,14 @@ class Study{
   String? id;
   String? name;
   int? level;
-  
+
   Study({this.id, this.name, this.level});
+
+  factory Study.fromJson(Map<String, dynamic> json){
+    return Study(
+      id: json['id'],
+      name: json['name'],
+      level: json['level'],
+    );
+  }
 }
