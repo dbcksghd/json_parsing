ddb45c69b981e810abf5fd9339c52865e4464dce
Yoochanhong <chanhongy6@naver.com>
2022-10-02 19:36:45 +0900

    간단한 Json 데이터 파싱해보기

--- simple_map/lib/main.dart
+++ simple_map/lib/main.dart
@@ -1,15 +1,81 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class Student {
-  String? id;
   String? name;
-  int? age;
 
-  Student({this.id, this.name, this.age});
+  Student({this.name});
 
-  factory Student.fromJson(Map<String, dynamic> json){
+  factory Student.fromJson(Map<String, dynamic> json) {
     return Student(
-      id: json['id'],
       name: json['name'],
-      age: json['age'],
+    );
+  }
+}
+
+Future<Student> getApi() async {
+  final response = await http.get(
+      Uri.parse('https://my-json-server.typicode.com/typicode/demo/profile'));
+  if (response.statusCode == 200) {
+    print(response.body);
+    return Student.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('실패');
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Student>? student;
+
+  void initState() {
+    super.initState();
+    student = getApi();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('간단한 json 파싱'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                getApi();
+              },
+              icon: Icon(Icons.refresh),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FutureBuilder(
+                future: student,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data!.name!);
+                  } else if (snapshot.hasError) {
+                    return Text('Error');
+                  } else
+                    return Text('예외');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
