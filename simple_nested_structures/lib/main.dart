82eeace26481481779811730a197591ec541a74e
Yoochanhong <chanhongy6@naver.com>
2022-10-04 20:29:40 +0900

    Futurebuilder 구현

--- simple_nested_structures/lib/main.dart
+++ simple_nested_structures/lib/main.dart
@@ -42,3 +42,61 @@ class Test {
   }
 }
 
+Future<Test> getApi() async {
+  final response = await http.get(
+      Uri.parse('https://my-json-server.typicode.com/lmodan/myjsonserver/db'));
+  print(response.body);
+  if (response.statusCode == 200){
+    return Test.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('실패');
+  }
+}
+
+void main() =>runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Test>? test;
+
+  @override
+  void initState(){
+    super.initState();
+    test = getApi();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FutureBuilder(
+                  future: test,
+                  builder: (context, snapshot){
+                    if (snapshot.hasData){
+                      return Text(snapshot.data!.slide!.name!);
+                    } else if (snapshot.hasError){
+                      return Text('에러');
+                    } else {
+                      return Text('실패');
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
