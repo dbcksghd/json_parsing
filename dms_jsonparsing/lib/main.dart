effb8e6d9372bfcb1741eded6cc7014af8bc0ea0
Yoochanhong <chanhongy6@naver.com>
2022-10-06 20:18:26 +0900

    dms Json 파싱 성공 ㅠㅠㅠ

--- dms_jsonparsing/lib/main.dart
+++ dms_jsonparsing/lib/main.dart
@@ -1,20 +1,26 @@
 import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+String getSystemTime() {
+  var now = DateTime.now();
+  return DateFormat("yyy-MM-dd").format(now);
+}
 class Test {
   Date? date;
 
   Test({this.date});
 
   Test.fromJson(Map<String, dynamic> json) {
-    date = json['date'] != null ? new Date.fromJson(json['date']) : null;
+    date = json['${getSystemTime()}'] != null ? new Date.fromJson(json['${getSystemTime()}']) : null;
+    print('${getSystemTime()}');
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     if (this.date != null) {
-      data['date'] = this.date!.toJson();
+      data['${getSystemTime()}'] = this.date!.toJson();
     }
     return data;
   }
@@ -43,14 +49,61 @@ class Date {
 }
 
 Future<Test> getApi() async {
-  var year = DateTime.now().year;
-  var month = DateTime.now().month;
-  var day = DateTime.now().day;
-  final response = await http.get(Uri.parse('https://api.dsm-dms.com/meal/$year-$month-$day'));
-  print(response.body);
-  if (response.statusCode == 200){
-    return Test.fromJson(json.decode(response.body));
+  final response = await http
+      .get(Uri.parse('https://api.dsm-dms.com/meal/${getSystemTime()}'));
+  if (response.statusCode == 200) {
+    print(json.decode(utf8.decode(response.bodyBytes)));
+    return Test.fromJson(json.decode(utf8.decode(response.bodyBytes)));
   } else {
     throw Exception('예외처리');
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Test>? test;
+
+  @override
+  void initState() {
+    super.initState();
+    test = getApi();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FutureBuilder(
+                  future: test,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data!.date!.breakfast![0].toString());
+                    } else if (snapshot.hasError) {
+                      return Text('에러');
+                    } else {
+                      return Text('실패');
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
