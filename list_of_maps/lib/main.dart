89376377ed25a93f9b68560ecff7f1483a8715c9
Yoochanhong <chanhongy6@naver.com>
2022-10-06 14:24:30 +0900

    객체로 이루어진 배열 Json 파싱 성공

--- list_of_maps/lib/main.dart
+++ list_of_maps/lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class Test {
   int? id;
   String? name;
@@ -17,5 +21,75 @@ class Test {
     data['name'] = this.name;
     data['username'] = this.username;
     return data;
+  }
+}
+
+class TestList{
+  final List<Test>? tests;
+  TestList({this.tests});
+  factory TestList.fromJson(List<dynamic> json){
+    List<Test> tests = <Test>[];
+    tests = json.map((i) => Test.fromJson(i)).toList();
+
+    return TestList(
+      tests: tests,
+    );
+  }
+}
+
+Future<TestList> getApi() async {
+  final response = await http.get(Uri.parse('https://my-json-server.typicode.com/Ygdrassil/cc/users'));
+  print(response.body);
+  if (response.statusCode == 200){
+    return TestList.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('예외처리');
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<TestList>? test;
+  @override
+  void initState(){
+    super.initState();
+    test = getApi();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FutureBuilder(
+                  future: test,
+                  builder: (context, snapshot){
+                    if (snapshot.hasData){
+                      return Text(snapshot.data!.tests![0].name.toString());
+                    } else if (snapshot.hasError){
+                      return Text('에러');
+                    } else {
+                      return Text('실패');
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
