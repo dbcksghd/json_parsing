488a05940315981570d39c1896871368a016f2fc
Yoochanhong <chanhongy6@naver.com>
2022-10-06 16:08:07 +0900

    api 받아올 Future 클래스 생성

--- dms_jsonparsing/lib/main.dart
+++ dms_jsonparsing/lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class Test {
   Date? date;
 
@@ -35,5 +39,18 @@ class Date {
     data['dinner'] = this.dinner;
     data['lunch'] = this.lunch;
     return data;
+  }
+}
+
+Future<Test> getApi() async {
+  var year = DateTime.now().year;
+  var month = DateTime.now().month;
+  var day = DateTime.now().day;
+  final response = await http.get(Uri.parse('https://api.dsm-dms.com/meal/$year-$month-$day'));
+  print(response.body);
+  if (response.statusCode == 200){
+    return Test.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('예외처리');
   }
 }
